648ca582520245897526f39b40e447ac9a3c705e
sparkling <henrik@sparklingideas.co.uk>
2022-04-22 16:54:57 +0100

    netlify setup

--- netlify/functions/deploy-succeeded.js
+++ netlify/functions/deploy-succeeded.js
@@ -1,4 +1,8 @@
 export async function handler(event, context) {
   console.log(event);
   console.log(context);
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Hello World" }),
+  };
 }
